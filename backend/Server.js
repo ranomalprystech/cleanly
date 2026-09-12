02b5cbc28e8b48b846375014d02f641f8e53015e
ranomalprystech <ranomal.prystech@gmail.com>
2026-09-12 05:49:40 +0500

    Update Server.js

--- backend/Server.js
+++ backend/Server.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 const app = express();
 app.use(cors({
   origin: [
-    "https://cleanlydemo.netlify.app/",
+    "https://cleanlydemo.netlify.app",
     "http://localhost:5173"
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
