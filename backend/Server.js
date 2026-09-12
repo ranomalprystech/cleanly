62d7fd55d1fee577937713a2dfeaca1c7b4d9972
ranomalprystech <ranomal.prystech@gmail.com>
2026-09-12 05:43:49 +0500

    Update Server.js

--- backend/Server.js
+++ backend/Server.js
@@ -6,7 +6,14 @@ const bcrypt = require('bcryptjs');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: [
+    "https://cleanlydemo.netlify.app/",
+    "http://localhost:5173"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 
 const User = require('./models/User');
